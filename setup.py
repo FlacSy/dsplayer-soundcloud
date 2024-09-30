9273419e60b74090a268059fc12ff7a2f3fe842e
FlacSy <flacsy.x@gmail.com>
2024-09-30 23:06:22 +0200

    Update for new version dsplayer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsplayer-soundcloud',  
-    version='1.0.0',
+    version='1.Update for new version dsplayer.0',
     packages=find_packages(),
     install_requires=[
         'dsplayer',
